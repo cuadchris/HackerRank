b4e6259e1260f3b731213bc79bd5c6440d95d34b
Christopher <cuadradochris@hotmail.com>
2023-09-07 15:34:51 -0400

    last of the basic join challenges

---
+++ SQL/Basic Join/Average_Population_of_Each_Continent.sql
@@ -0,0 +1,7 @@
+https: / / www.hackerrank.com / challenges / average - population - of - each - continent / problem
+
+select continent,
+    floor(avg(city.population))
+from country
+    join city on countrycode = code
+group by continent
---
+++ SQL/Basic Join/Population_Census.sql
@@ -0,0 +1,6 @@
+https: / / www.hackerrank.com / challenges / asian - population / problem
+
+SELECT SUM(city.population)
+FROM city
+    JOIN country ON countrycode = code
+WHERE continent = 'Asia'
